bd626b3641a29b291021c3cfa349bdd56f5621ba
Paul Tagliamonte <tag@pault.ag>
2014-08-07 14:43:38 -0400

    do upgrade.

---
+++ deploy/upgrade.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /etc/docker/moxie.sh
+
+function moxierun {
+    docker run --rm -it \
+        -v /srv/lucifer.pault.ag/prod/moxie:/moxie \
+        -e DATABASE_URL=${DATABASE_URL} \
+        paultag/moxie \
+        $@
+}
+
+cd ~tag/projects/moxie
+git pull
+docker build --rm -t paultag/moxie .
+
+sudo service moxie stop
+sudo service moxied stop
+
+docker rm moxie
+docker rm moxied
+moxierun alembic upgrade head
+
+sudo service moxie start
+sudo service moxied start
+sudo service nginx restart
