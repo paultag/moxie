af6edbe020966696aafd5012ced25520bc7f3fd6
Paul Tagliamonte <tag@pault.ag>
2014-08-08 10:56:04 -0400

    Let's try sockets.

--- deploy/moxie.sh
+++ deploy/moxie.sh
@@ -2,5 +2,7 @@
 
 DATABASE_URL=postgis://moxie:moxie@postgres.prod.lucifer.pault.ag:5432/moxie
 
-MOXIE_HOST="0.0.0.0"
-MOXIE_PORT="8888"
+# MOXIE_HOST="0.0.0.0"
+# MOXIE_PORT="8888"
+
+MOXIE_SOCKET="/sockets/moxie.sock"
