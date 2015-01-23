cc74538627485beccf65fde7ab39d16f809a6cf6
Paul Tagliamonte <tag@pault.ag>
2015-01-23 00:17:57 -0500

    2222

--- moxie/cores/ssh.py
+++ moxie/cores/ssh.py
@@ -280,7 +280,7 @@ class SSHService(Service):
             MoxieSSHServer._keys = authorized_keys
 
         obj = yield from asyncssh.create_server(
-            MoxieSSHServer, 'localhost', 2222,
+            MoxieSSHServer, '0.0.0.0', 2222,
             server_host_keys=ssh_host_keys
         )
 
