1fd9749ff801695d8651f5855928df0fee8d5c8f
Paul Tagliamonte <tag@pault.ag>
2015-01-23 00:13:49 -0500

    Update integration

--- moxie/butterfield.py
+++ moxie/butterfield.py
@@ -1,6 +1,9 @@
+import os
 import asyncio
 from butterfield.utils import at_bot
 from aiocore import Service
+
+WEB_ROOT = os.environ.get("MOXIE_WEB_URL", "http://localhost:8888")
 
 @asyncio.coroutine
 @at_bot
@@ -16,8 +19,15 @@ def run(bot, message: "message"):
         job = arg
         yield from bot.post(
             message['channel'], "Doing bringup of {}".format(job))
-        yield from runner.run(job)
-        yield from bot.post(
-            message['channel'],
-            "Job {} online - http://localhost:8888/container/{}/".format(job, job)
-        )
+        try:
+            yield from runner.run(job)
+        except ValueError as e:
+            yield from bot.post(
+                message['channel'],
+                "Gah, {job} failed - {e}".format(e=e, job=job)
+            )
+            return
+
+        yield from bot.post(message['channel'],
+            "Job {job} online - {webroot}/container/{job}/".format(
+                webroot=WEB_ROOT, job=job))
