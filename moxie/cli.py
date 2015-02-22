4cc3ded13e9bcce5b1b26623a58a71296a5840f1
Paul Tagliamonte <tag@pault.ag>
2015-02-21 22:00:27 -0500

    update

--- moxie/cli.py
+++ moxie/cli.py
@@ -67,7 +67,6 @@ def serve():
         os.environ.get("MOXIE_SMTP_USER", None),
         os.environ.get("MOXIE_SMTP_PASS", None),
     ))
-    # loop.run_until_complete(alert.failure("test", 1))
 
     socket_fp = os.environ.get("MOXIE_SOCKET", None)
     if socket_fp:
